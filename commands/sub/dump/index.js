ee597f3bf2fd7c68b62a2fffc97e0c9cf06a3029
Petko D. Petkov <pdp@gnucitizen.org>
2019-05-30 09:59:47 +0000

    Output more meta-data.

--- commands/sub/dump/index.js
+++ commands/sub/dump/index.js
@@ -16,33 +16,48 @@ exports.yargs = {
         const mkdir = util.promisify(fs.mkdir)
         const writeFile = util.promisify(fs.writeFile)
 
+        const save = async(pathname, data) => {
+            await mkdir(path.dirname(pathname), { recursive: true })
+            await writeFile(pathname, data)
+        }
+
+        const dumps = (object) => {
+            return JSON.stringify(object, '', '  ')
+        }
+
         const repositoryPages = await listRepositories(owner)
+
+        const ownerPrefix = path.join(owner)
+
+        await save(path.join(ownerPrefix, 'repository-pages.json'), dumps(repositoryPages))
 
         await Promise.all(repositoryPages.map(async({ repositories }) => {
             await Promise.all(repositories.map(async({ id: repositoryId }) => {
                 const repositoryRequestPages = await listRepositoryRequests(repositoryId)
 
+                const repositoryPrefix = path.join(ownerPrefix, `${repositoryId}`)
+
+                await save(path.join(repositoryPrefix, 'request-pages.json'), dumps(repositoryRequestPages))
+
                 await Promise.all(repositoryRequestPages.map(async({ requests }) => {
                     await Promise.all(requests.map(async({ id: requestId }) => {
                         const request = await getRepositoryRequest(repositoryId, requestId)
 
-                        const requestPrefix = path.join(owner, repositoryId.toString(), requestId.toString())
+                        const requestPrefix = path.join(repositoryPrefix, `${requestId}`)
 
-                        await mkdir(requestPrefix, { recursive: true })
+                        await save(path.join(requestPrefix, 'request.json'), dumps(request))
 
                         await Promise.all(request.raw_configs.map(async({ config }, id) => {
-                            await writeFile(path.join(requestPrefix, `${id}.yaml`), config)
+                            await save(path.join(requestPrefix, `config-${id}.yaml`), config)
                         }))
 
                         await Promise.all(request.builds.map(async({ jobs }) => {
                             await Promise.all(jobs.map(async({ id: jobId }) => {
-                                const jobPrefix = path.join(requestPrefix, jobId.toString())
-
-                                await mkdir(jobPrefix, { recursive: true })
-
                                 const log = await getJobLog(jobId)
 
-                                await writeFile(path.join(jobPrefix, 'log.txt'), log)
+                                const jobPrefix = path.join(requestPrefix, `${jobId}`)
+
+                                await save(path.join(jobPrefix, 'log.txt'), log)
                             }))
                         }))
                     }))
